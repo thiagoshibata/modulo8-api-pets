3a1b0b503b546fdeb19f5cdad34d3b9cb145e5d2
thiago-shibata <thiago.shibatta@gmail.com>
2024-12-09 09:35:21 -0300

    feat: implementing the rest of the views

---
+++ src/views/pets_deleter_view.py
@@ -0,0 +1,14 @@
+from src.controllers.interfaces.pets_deleter_controller import PetsDeleterControllerInterface
+from .interfaces.view_interface import ViewInterface
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+
+class PersonCreatorView(ViewInterface):
+    def __init__(self, controller: PetsDeleterControllerInterface) -> None:
+        self.__controller = controller
+
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        name = http_request.param["name"]
+        self.__controller.delete(name)
+
+        return HttpResponse(status_code=204)
